56e5d6172d90703ab12f8ab3a2ceee2ff6d643cb
hitman472005 <103201022+hitman472005@users.noreply.github.com>
2023-08-06 20:15:47 -0400

    t15a2
    t15a2

---
+++ tarea 15.2 Resta/resta xd/restar c++ xd/restar c++ xd.cpp
@@ -0,0 +1,53 @@
+// restar c++ xd.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+
+using namespace std; 
+
+int main()
+{
+
+    int n1, n2, restar;
+
+    cout << "**************************\n";
+    cout << "*   Angel Steven #16     *\n";
+    cout << "**************************\n";
+
+    cout << " \n";
+
+    cout << " Restar xd \n";
+
+    cout << " \n";
+
+
+    cout << " introduce el primer numero\n";
+
+    cin >> n1;
+
+    cout << " \n";
+
+    cout << " introduce el segundo numero\n";
+
+    cin >> n2;
+
+    cout << " \n";
+
+    restar = n1 - n2;
+
+    cout << "El resultado de esta suma es: " << restar << endl;
+
+    system("pause");
+    return 0;
+
+}
+
+// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
+// Depurar programa: F5 o menú Depurar > Iniciar depuración
+
+// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
+//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
+//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
+//   4. Use la ventana Lista de errores para ver los errores
+//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
+//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
