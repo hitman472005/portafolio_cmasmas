795996f1bb151a0c425e868b8f4ffd8592ee710b
hitman472005 <103201022+hitman472005@users.noreply.github.com>
2023-08-06 20:10:24 -0400

    t12
    t12

---
+++ Tarea 12 C++ lineas de texto en consola/En consola C++/En consola C++.cpp
@@ -0,0 +1,22 @@
+// En consola C++.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << " 1 *****************************************\n";
+    std::cout << " 2 *  Bienaventurados los mansos, porque   *\n";
+    std::cout << " 3 *  ellos poseeran en herencia la tierra.*\n";
+    std::cout << " 4 *****************************************\n";
+}
+
+// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
+// Depurar programa: F5 o menú Depurar > Iniciar depuración
+
+// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
+//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
+//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
+//   4. Use la ventana Lista de errores para ver los errores
+//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
+//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
